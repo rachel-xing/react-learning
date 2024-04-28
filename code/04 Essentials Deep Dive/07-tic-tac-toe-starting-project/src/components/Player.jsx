1e4251c021f3fae61bea91f26d104ae73ca91555
xing0023 <xing0023@algonquinlive.com>
2024-04-28 15:53:27 -0400

    [UDEMY-MAXIM][Rachel] create Player and GameBoard component

---
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,31 @@
+import {useState} from "react";
+
+export default function Player({initialName,symbol,isActive}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing,setIsEditing ] = useState(false);
+
+  function handleEditClick() {
+    // isEditing ? setIsEditing(false) : setIsEditing(true)
+    // setIsEditing(!isEditing)
+    setIsEditing(Editing => !Editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if (isEditing) {
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
+  };
+
+  return (
+    <li className={isActive? 'active' : ''}>
+        <span className="player">
+          {editablePlayerName}
+        </span>
+      <span className='player-symbol'>{symbol}</span>
+      <button onClick={handleEditClick}>{isEditing? "Save" : 'Edit'}</button>
+    </li>
+  )
+}
