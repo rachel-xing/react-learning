32fe7c41515ac56e9c89641aa6d6c0c8c5e5f93f
xing0023 <xing0023@algonquinlive.com>
2024-04-30 14:23:42 -0400

    [UDEMY-MAXIM][Rachel] finish Log component

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,20 +1,26 @@
-import {useState} from "react";
 const initialGameBoard = [
   [null,null,null],
   [null,null,null],
   [null,null,null]
-]
-export default function GameBoard({onSelectedSquare,activePlayerSymbol}) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  function handleSelectSquare(rowIndex,colIndex) {
-    setGameBoard(prevGameBoard=> {
-      const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol
-      return updatedGameBoard
-    } )
+];
+export default function GameBoard({onSelectedSquare,turns}) {
+  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  // function handleSelectSquare(rowIndex,colIndex) {
+  //   setGameBoard(prevGameBoard=> {
+  //     const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol
+  //     return updatedGameBoard
+  //   } )
+  //
+  //   onSelectedSquare()
+  //
+  // }
 
-    onSelectedSquare()
-
+let gameBoard = initialGameBoard
+  for (const turn of turns) {
+   const  {square,player} = turn;
+   const {row,col} = square;
+    gameBoard[row][col] = player
   }
 
   return <ol id="game-board">
@@ -23,7 +29,7 @@ export default function GameBoard({onSelectedSquare,activePlayerSymbol}) {
       <ol>
         {row.map((playerSymbol,colIndex)=>
           <li key={colIndex}>
-            <button onClick={() => handleSelectSquare(rowIndex,colIndex)}>
+            <button onClick={()=> onSelectedSquare(rowIndex,colIndex)}>
               {playerSymbol}
             </button>
           </li>)}
