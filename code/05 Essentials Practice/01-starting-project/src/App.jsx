06651b37553e64c39bf9560bdffb5a43da897b70
xing0023 <xing0023@algonquinlive.com>
2024-05-01 16:20:31 -0400

    [UDEMY-MAXIM][Rachel] feat: add UserInput component

--- code/05 Essentials Practice/01-starting-project/src/App.jsx
+++ code/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -1,8 +1,15 @@
 import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+
 function App() {
   return (
     // <h1>React Investment Calculator</h1>
-    <Header/>
+    <>
+      <Header/>
+      <UserInput/>
+    </>
+
+
 
   );
 }
