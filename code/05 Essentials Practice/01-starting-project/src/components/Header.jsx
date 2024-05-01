da2bd43a054c259eedc7eb550c9c1d68b905d43a
xing0023 <xing0023@algonquinlive.com>
2024-05-01 15:42:21 -0400

    [UDEMY-MAXIM][Rachel] feat: add Header Component

---
+++ code/05 Essentials Practice/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header id="header">
+      <img src="../../public/investment-calculator-logo.png" alt=""/>
+      <h1>Investment Calculator</h1>
+    </header>
+
+  );
+}
+
+export default Header;
