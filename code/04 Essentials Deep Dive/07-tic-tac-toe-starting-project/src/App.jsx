fdddedd37855efd1a638b356f9853f9a01333864
xing0023 <xing0023@algonquinlive.com>
2024-05-03 12:22:38 -0400

    [UDEMY-MAXIM][Rachel] feat: add Log GameOver

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,8 +1,10 @@
 import {useState} from "react";
 import {PLAYERS, SYMBOL, INITIAL_GAME_BOARD} from "./utils.js";
+import {WINNING_COMBINATIONS} from "./winning-combinations.js";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
+import GameOver from "./components/GameOver.jsx";
 
 function deriveGameBoard(gameTurns) {
   const gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
@@ -22,13 +24,29 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer
 }
 
+function deriveWinner(gameBoard,player) {
+  let winner;
+  for(const combination of WINNING_COMBINATIONS) {
+    let first = gameBoard[combination[0].row][combination[0].column];
+    let second = gameBoard[combination[1].row][combination[1].column];
+    let third = gameBoard[combination[2].row][combination[2].column];
+
+    if(first && first === second && first === third) {
+      winner = player[first];
+    }
+  }
+  return winner;
+}
+
 
 
 function App() {
-  const [players, setPlayer] = useState(PLAYERS)
-  const [gameTurns, setGameTurns] = useState([])
-  const gameBoard = deriveGameBoard(gameTurns)
-  const activePlayer = deriveActivePlayer(gameTurns)
+  const [players, setPlayer] = useState(PLAYERS);
+  const [gameTurns, setGameTurns] = useState([]);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const winner = deriveWinner(gameBoard,players);
+  const hasDraw = gameTurns.length === 9 && !winner
 
   function handleNameChange(symbol, newName) {
     setPlayer(prevPlayer => ({
@@ -54,6 +72,9 @@ function App() {
     })
   }
 
+  function handleRematch(gameTurns) {
+    setGameTurns([])
+  }
   return (
     <main>
       <div id="game-container">
@@ -71,6 +92,7 @@ function App() {
           />
         </ol>
 
+        {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch}/>}
         <GameBoard
           board={gameBoard}
           onSelectSquare={handleSelectSquare}
