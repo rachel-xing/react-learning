c2b64a4706adf0f6f6522274e98a10c80f645087
xing0023 <xing0023@algonquinlive.com>
2024-05-05 14:44:17 -0400

    [UDEMY-MAXIM][Rachel] feat: add Player component

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,107 +1,30 @@
-import {useState} from "react";
-import {PLAYERS, SYMBOL, INITIAL_GAME_BOARD} from "./utils.js";
-import {WINNING_COMBINATIONS} from "./winning-combinations.js";
+import {SYMBOL,PLAYERS} from "./utils.js";
 import Player from "./components/Player.jsx";
-import GameBoard from "./components/GameBoard.jsx";
-import Log from "./components/Log.jsx";
-import GameOver from "./components/GameOver.jsx";
-
-function deriveGameBoard(gameTurns) {
-  const gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
-  for (const turn of gameTurns) {
-    const {square, player} = turn;
-    const {row, col} = square;
-    gameBoard[row][col] = player;
-  }
-  return gameBoard;
-}
-
-function deriveActivePlayer(gameTurns) {
-  let currentPlayer = "X";
-  if (gameTurns.length && gameTurns[0].player === "X") {
-    currentPlayer = "O"
-  }
-  return currentPlayer
-}
-
-function deriveWinner(gameBoard,player) {
-  let winner;
-  for(const combination of WINNING_COMBINATIONS) {
-    let first = gameBoard[combination[0].row][combination[0].column];
-    let second = gameBoard[combination[1].row][combination[1].column];
-    let third = gameBoard[combination[2].row][combination[2].column];
-
-    if(first && first === second && first === third) {
-      winner = player[first];
-    }
-  }
-  return winner;
-}
-
-
+import {useState} from "react";
 
 function App() {
-  const [players, setPlayer] = useState(PLAYERS);
-  const [gameTurns, setGameTurns] = useState([]);
-  const gameBoard = deriveGameBoard(gameTurns);
-  const activePlayer = deriveActivePlayer(gameTurns);
-  const winner = deriveWinner(gameBoard,players);
-  const hasDraw = gameTurns.length === 9 && !winner
-
-  function handleNameChange(symbol, newName) {
-    setPlayer(prevPlayer => ({
-      ...prevPlayer,
-      [symbol]: newName,
-
-    }))
-  }
-
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameTurns(prevTurns => {
-      const currentPlayer = deriveActivePlayer(prevTurns);
-      return [
-        {
-          square: {
-            row: rowIndex,
-            col: colIndex
-          },
-          player: currentPlayer
-        },
-        ...prevTurns
-      ];
+  const [players,setPlayers] = useState(PLAYERS)
+  function handleNameUpdate(symbol,newName) {
+    setPlayers(prevPlayers => {
+      return {
+      ...prevPlayers,
+      [symbol]: newName
+      }
     })
   }
 
-  function handleRematch(gameTurns) {
-    setGameTurns([])
-  }
   return (
     <main>
       <div id="game-container">
-
-        <ol id="players" className="highlight-player">
-          <Player
-            symbol={SYMBOL.X}
-            onChange={handleNameChange}
-            isActive={activePlayer === 'X'}
-          />
-          <Player
-            symbol={SYMBOL.O}
-            onChange={handleNameChange}
-            isActive={activePlayer === "O"}
-          />
+        <ol id="players">
+          <Player symbol={SYMBOL.X} onNameUpdate={handleNameUpdate}/>
+          <Player symbol={SYMBOL.O} onNameUpdate={handleNameUpdate}/>
         </ol>
 
-        {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch}/>}
-        <GameBoard
-          board={gameBoard}
-          onSelectSquare={handleSelectSquare}
-        />
       </div>
-      <Log turns={gameTurns}/>
+
+
     </main>
-
-
   )
 }
 
--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,59 +1,47 @@
 import {useState} from "react";
 import {PLAYERS} from "../utils.js";
 
-function Player({symbol,onChange,isActive}) {
-  const [isEditing,setIsEditing] =useState(false)
-  const initialName = PLAYERS[symbol]
-  const [playerName,setPlayerName] = useState(initialName)
+function Player({symbol,onNameUpdate}) {
+  const [playerName,setPlayerName] = useState(PLAYERS[symbol])
+  const [isEditing,setIsEditing] = useState(false)
 
   function handleClick() {
-    setIsEditing(editing=>!editing)
-
-    if(isEditing) {
-      onChange(symbol,playerName)
-    }
+    setIsEditing(editing=> !editing)
   }
 
   function handleChange(event) {
     setPlayerName(event.target.value)
+    if(isEditing) {
+      onNameUpdate(symbol,playerName)
+    }
   }
 
+  if(isEditing) {
+    return (
+      <li>
+      <span className="player">
+        <input type="text" value={playerName} onChange={handleChange}/>
+      </span>
+        <span className="player-symbol">
+        {symbol}
+      </span>
+        <button onClick={handleClick}>Save</button>
+      </li>
+    )}
 
-   if (!isEditing) {
-     return(
-       <li className={isActive ? 'active' : ''}>
-         <span className="player">
-           <span className="player-name">
-             {playerName}
-           </span>
-         </span>
-         <span className="player-symbol">
-           {symbol}
-         </span>
-         <button onClick={handleClick}>
-           Edit
-         </button>
-       </li>
-     )}
-
-   return (
-     <li className={isActive ? 'active' : ''}>
-       <span className="player">
-         <input
-           type="text"
-           value={playerName}
-           required
-           onChange={handleChange}
-         />
-       </span>
-       <span className="player-symbol">
-         {symbol}
-       </span>
-       <button onClick={handleClick}>
-         Save
-       </button>
-     </li>
-   )
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">
+          {playerName}
+        </span>
+      </span>
+      <span className="player-symbol">
+        {symbol}
+      </span>
+      <button onClick={handleClick}>Edit</button>
+    </li>
+  )
 }
 
 export default Player;
