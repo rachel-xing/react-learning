0b51ae0f8c8bad8f3afe4ba280c336d2cd70a36f
Rachel Shing <xing0023@algonquinlive.com>
2024-05-08 11:25:42 -0400

    [UDEMY-MAXIM][Rachel] style: split index.css to styled components

--- code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
+++ code/07 Styling/01-starting-project/src/components/AuthInputs.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import {styled} from 'styled-components';
+import Button from "./Button.jsx";
+import CustomizedInput from "./Input.jsx";
 
 const ControlContainer = styled.div`
   display: flex;
@@ -7,26 +9,9 @@ const ControlContainer = styled.div`
   gap: 0.5rem;
   margin-bottom: 1.5rem;`
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: #6b7280;
-`
 
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+
+
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -50,37 +35,28 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          <Label className={ `'label' ${emailNotValid ? 'invalid' : ''}`}>Email</Label>
-          <Input
+          <CustomizedInput
             type="email"
-            style={{
-              backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
-            }}
-            // className={emailNotValid ? 'invalid' : undefined}
+            label="Email"
+            $invalid={emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
-        </p>
-        <p>
-          <Label className={ `'label' ${passwordNotValid ? 'invalid' : ''}`}>Password</Label>
-          <Input
+
+          <CustomizedInput
             type="password"
-            style={{
-              backgroundColor: passwordNotValid ? '#fed2d2' : '#d1d5db'
-            }}
-            // className={passwordNotValid ? 'invalid' : undefined}
+            label="Password"
+            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
-        </p>
       </ControlContainer>
 
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
