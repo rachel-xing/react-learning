c8e5e4646d8255c66122781c0afdf5652c48a739
Rachel Shing <xing0023@algonquinlive.com>
2024-06-17 11:42:24 -0400

    [UDEMY-MAXIM][Rachel] add Meals and MealItem component

---
+++ code/18 Practice Project - Food Order/01-starting-project/src/components/Meals.jsx
@@ -0,0 +1,39 @@
+import {useEffect, useState} from "react";
+import {fetchAvailableMeals} from "../http.js";
+import MealItem from "./MealItem.jsx";
+export default function Meals() {
+    const [availableMeals, setAvailableMeals] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
+
+    useEffect(() => {
+            async function fetchMeals() {
+                setIsFetching(true);
+                const response = await fetch("http://localhost:3000/meals");
+
+                if (!response.ok) {
+                    throw new Error("Failed to  fetch meals");
+                }
+
+                const meals = await response.json();
+                setAvailableMeals(meals);
+            }
+
+            fetchMeals();
+            setIsFetching(false);
+
+        }, []
+    )
+
+    console.log(availableMeals)
+
+    return (
+        <ul id="meals">
+            {isFetching && <p>Fetching data...</p>}
+            {availableMeals.map(meal =>
+                <MealItem key={meal.id} meal={meal}/>
+            )}
+        </ul>
+    );
+
+
+}
